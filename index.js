855768923ced6eaf4b2b2b71faf8d2d0a6eac1ff
Koury Lape <lapek@denison.edu>
2018-05-18 12:18:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict'
+const backgroundColor = '#282a36'
+const foregroundColor = '#f6f6f4'
+const black = '#4f4f4f'
+const red = '#c54843'
+const green = '#48bd60'
+const yellow = '#bec276'
+const blue = '#9d82bc'
+const magenta = '#ca699b'
+const cyan = '#7db7c0'
+const white = '#cfcfcf'
+const brightBlack = '#747474'
+const brightRed = "#ee6666"
+const brightGreen = "#62e884"
+const brightYellow = "#e7ee98"
+const brightBlue = "#bf9eee"
+const brightMagenta = "#f286c4"
+const brightCyan = "#97e1f1"
+const brightWhite = "#f6f6f4"
+const tabActive = '#44475a'
+
+exports.decorateConfig = config => {
+	return Object.assign({}, config, {
+	  backgroundColor,
+	  foregroundColor,
+	  borderColor: black,
+	  cursorColor: brightBlack,
+	  colors: [
+		black,
+		red,
+		green,
+		yellow,
+		blue,
+		magenta,
+		cyan,
+		white,
+		// bright
+		brightBlack,
+		brightRed,
+		brightGreen,
+		brightYellow,
+		brightBlue,
+		brightMagenta,
+		brightCyan,
+		brightWhite
+	  ],
+	  css: `
+			  ${config.css || ''}
+			  .tabs_list .tab_tab.tab_active .tab_text  {
+				  background: ${backgroundColor};
+			  }
+
+			  .tab_active:before {
+				  border-color: ${tabActive};
+			  }
+		  `
+	})
+  }
+
+
+
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Dracula for [Hyper](https://hyper.is)
+
+> A spin off of the [Dracula theme](https://draculatheme.com/) for [Hyper](https://hyper.is).
+
+![Screenshot](screenshot.png)
+
+## Install
+
+Add `hyper-dracula-soft` to the plugins list in your ~/.hyper.js config file.
+
+## Special Thanks
+
+[Dracula](https://github.com/dracula/dracula-theme/) is a project created by Zeno Rocha with the help of many awesome contributors and maintainers.
+
+## License
+
+[MIT License](./LICENSE)
